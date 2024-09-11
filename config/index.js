0d9d2bd1e0229b99ac60171378c1afd67fd54f5c
huanghui <huanghui0906@163.com>
2024-09-11 08:51:27 +0800

    更改build目录

--- config/index.js
+++ config/index.js
@@ -52,7 +52,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     // assetsPublicPath: './',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
